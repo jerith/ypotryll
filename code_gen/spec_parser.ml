af8b0a5578b32a5c593887536f710a5fd0f5e541
Jeremy Thurgood <firxen@gmail.com>
2014-07-29 23:21:20 +0200

    Consolidate element parsers.

--- code_gen/spec_parser.ml
+++ code_gen/spec_parser.ml
@@ -46,11 +46,11 @@ let swallow_element f collected elems = function
   | E (tag, attrs, _) -> failwith (Printf.sprintf "bad tag: %s" (fmt_tag tag attrs))
 
 let rec consume_children children =
-  let rec inner () = function
+  let rec parse_children () = function
     | [] -> ()
-    | elem :: elems -> swallow_element inner () elems elem
+    | elem :: elems -> swallow_element parse_children () elems elem
   in
-  inner () children
+  parse_children () children
 
 
 let parse_constant attrs children =
@@ -71,24 +71,21 @@ let parse_assert attrs children =
   let () = consume_children children in
   Assert.make check value
 
-let parse_domain_children domain elems =
-  let open Domain in
-  let rec inner domain = function
-    | [] -> domain
-    | E ("rule", attrs, children) :: elems ->
-      inner (add_rule domain @@ parse_rule attrs children) elems
-    | E ("assert", attrs, children) :: elems ->
-      inner (add_assert domain @@ parse_assert attrs children) elems
-    | elem :: elems -> swallow_element inner domain elems elem
-  in
-  inner domain @@ List.rev elems
-
 let parse_domain attrs children =
   let name = get_attr "name" attrs in
   let data_type = get_attr "type" attrs in
   let label = get_attr_option "label" attrs in
   let domain = Domain.make name data_type label in
-  parse_domain_children domain children
+  let open Domain in
+  let rec parse_children domain = function
+    | [] -> domain
+    | E ("rule", attrs, children) :: elems ->
+      parse_children (add_rule domain @@ parse_rule attrs children) elems
+    | E ("assert", attrs, children) :: elems ->
+      parse_children (add_assert domain @@ parse_assert attrs children) elems
+    | elem :: elems -> swallow_element parse_children domain elems elem
+  in
+  parse_children domain @@ List.rev children
 
 let parse_chassis attrs children =
   let name = get_attr "name" attrs in
@@ -96,48 +93,27 @@ let parse_chassis attrs children =
   let () = consume_children children in
   Chassis.make name implement
 
-let parse_field_children field elems =
-  let open Field in
-  let rec inner field = function
-    | [] -> field
-    | E ("rule", attrs, children) :: elems ->
-      inner (add_rule field @@ parse_rule attrs children) elems
-    | E ("assert", attrs, children) :: elems ->
-      inner (add_assert field @@ parse_assert attrs children) elems
-    | elem :: elems -> swallow_element inner field elems elem
-  in
-  inner field @@ List.rev elems
-
 let parse_field attrs children =
   let name = get_attr "name" attrs in
   let domain = get_attr_option "domain" attrs in
   let data_type = get_attr_option "type" attrs in
   let label = get_attr_option "label" attrs in
   let field = Field.make name domain data_type label in
-  parse_field_children field children
+  let open Field in
+  let rec parse_children field = function
+    | [] -> field
+    | E ("rule", attrs, children) :: elems ->
+      parse_children (add_rule field @@ parse_rule attrs children) elems
+    | E ("assert", attrs, children) :: elems ->
+      parse_children (add_assert field @@ parse_assert attrs children) elems
+    | elem :: elems -> swallow_element parse_children field elems elem
+  in
+  parse_children field @@ List.rev children
 
 let parse_response attrs children =
   let name = get_attr "name" attrs in
   let () = consume_children children in
   Response.make name
-
-let parse_method_children meth elems =
-  let open Method in
-  let rec inner meth = function
-    | [] -> meth
-    | E ("rule", attrs, children) :: elems ->
-      inner (add_rule meth @@ parse_rule attrs children) elems
-    | E ("chassis", attrs, children) :: elems ->
-      inner (add_chassis meth @@ parse_chassis attrs children) elems
-    | E ("response", attrs, children) :: elems ->
-      inner (add_response meth @@ parse_response attrs children) elems
-    | E ("field", attrs, children) :: elems ->
-      inner (add_field meth @@ parse_field attrs children) elems
-    | E ("assert", attrs, children) :: elems ->
-      inner (add_assert meth @@ parse_assert attrs children) elems
-    | elem :: elems -> swallow_element inner meth elems elem
-  in
-  inner meth @@ List.rev elems
 
 let parse_method attrs children =
   let name = get_attr "name" attrs in
@@ -146,23 +122,22 @@ let parse_method attrs children =
   let content = get_bool_attr "content" attrs in
   let label = get_attr_option "label" attrs in
   let meth = Method.make name index synchronous content label in
-  parse_method_children meth children
-
-let parse_class_children cls elems =
-  let open Class in
-  let rec inner cls = function
-    | [] -> cls
-    | E ("chassis", attrs, children) :: elems ->
-      inner (add_chassis cls @@ parse_chassis attrs children) elems
-    | E ("method", attrs, children) :: elems ->
-      inner (add_method cls @@ parse_method attrs children) elems
+  let open Method in
+  let rec parse_children meth = function
+    | [] -> meth
     | E ("rule", attrs, children) :: elems ->
-      inner (add_rule cls @@ parse_rule attrs children) elems
+      parse_children (add_rule meth @@ parse_rule attrs children) elems
+    | E ("chassis", attrs, children) :: elems ->
+      parse_children (add_chassis meth @@ parse_chassis attrs children) elems
+    | E ("response", attrs, children) :: elems ->
+      parse_children (add_response meth @@ parse_response attrs children) elems
     | E ("field", attrs, children) :: elems ->
-      inner (add_field cls @@ parse_field attrs children) elems
-    | elem :: elems -> swallow_element inner cls elems elem
+      parse_children (add_field meth @@ parse_field attrs children) elems
+    | E ("assert", attrs, children) :: elems ->
+      parse_children (add_assert meth @@ parse_assert attrs children) elems
+    | elem :: elems -> swallow_element parse_children meth elems elem
   in
-  inner cls @@ List.rev elems
+  parse_children meth @@ List.rev children
 
 let parse_class attrs children =
   let name = get_attr "name" attrs in
@@ -170,21 +145,20 @@ let parse_class attrs children =
   let index = get_int_attr "index" attrs in
   let label = get_attr "label" attrs in
   let cls = Class.make name handler index label in
-  parse_class_children cls children
-
-let parse_amqp_children spec elems =
-  let open Spec in
-  let rec inner spec = function
-    | [] -> spec
-    | E ("constant", attrs, children) :: elems ->
-      inner (add_constant spec @@ parse_constant attrs children) elems
-    | E ("domain", attrs, children) :: elems ->
-      inner (add_domain spec @@ parse_domain attrs children) elems
-    | E ("class", attrs, children) :: elems ->
-      inner (add_class spec @@ parse_class attrs children) elems
-    | elem :: elems -> swallow_element inner spec elems elem
+  let open Class in
+  let rec parse_children cls = function
+    | [] -> cls
+    | E ("chassis", attrs, children) :: elems ->
+      parse_children (add_chassis cls @@ parse_chassis attrs children) elems
+    | E ("method", attrs, children) :: elems ->
+      parse_children (add_method cls @@ parse_method attrs children) elems
+    | E ("rule", attrs, children) :: elems ->
+      parse_children (add_rule cls @@ parse_rule attrs children) elems
+    | E ("field", attrs, children) :: elems ->
+      parse_children (add_field cls @@ parse_field attrs children) elems
+    | elem :: elems -> swallow_element parse_children cls elems elem
   in
-  inner spec @@ List.rev elems
+  parse_children cls @@ List.rev children
 
 let parse_amqp attrs children =
   let major = get_int_attr "major" attrs in
@@ -192,7 +166,18 @@ let parse_amqp attrs children =
   let revision = get_int_attr "revision" attrs in
   let comment = get_attr "comment" attrs in
   let spec = Spec.make (major, minor, revision) comment in
-  parse_amqp_children spec children
+  let open Spec in
+  let rec parse_children spec = function
+    | [] -> spec
+    | E ("constant", attrs, children) :: elems ->
+      parse_children (add_constant spec @@ parse_constant attrs children) elems
+    | E ("domain", attrs, children) :: elems ->
+      parse_children (add_domain spec @@ parse_domain attrs children) elems
+    | E ("class", attrs, children) :: elems ->
+      parse_children (add_class spec @@ parse_class attrs children) elems
+    | elem :: elems -> swallow_element parse_children spec elems elem
+  in
+  parse_children spec @@ List.rev children
 
 let parse_spec (_dtd, tree) =
   match tree with

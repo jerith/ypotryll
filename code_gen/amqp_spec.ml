d375084e0cac92929645c92bf51d95e2e4aca9ac
Jeremy Thurgood <firxen@gmail.com>
2014-07-29 19:28:45 +0200

    More spec parsing work.

--- code_gen/amqp_spec.ml
+++ code_gen/amqp_spec.ml
@@ -11,6 +11,10 @@ let fmt_list name formatter ppf list =
   fmt_items list;
   Format.fprintf ppf "@;<1 -2>@]]"
 
+let fmt_optional_str name ppf = function
+  | None -> ()
+  | Some value -> Format.fprintf ppf "@ %s=%S" name value
+
 
 type constant = {
   name : string;
@@ -22,11 +26,8 @@ let make_constant name value cls =
   { name; value; cls }
 
 let fmt_constant ppf constant =
-  let cls = match constant.cls with
-    | None -> ""
-    | Some cls -> Printf.sprintf " (%s)" cls
-  in
-  Format.fprintf ppf "@[<4>%s =@ %d%s@]" constant.name constant.value cls
+  Format.fprintf ppf "@[<h><constant %s %d%a>@]"
+    constant.name constant.value (fmt_optional_str "class") constant.cls
 
 
 type assertion = {
@@ -38,11 +39,8 @@ let make_assertion check value =
   { check; value }
 
 let fmt_assertion ppf assertion =
-  let value = match assertion.value with
-    | None -> ""
-    | Some value -> Printf.sprintf " %S" value
-  in
-  Format.fprintf ppf "<assert %s%s>" assertion.check value
+  Format.fprintf ppf "@[<h><assert %s%a>@]"
+    assertion.check (fmt_optional_str "value") assertion.value
 
 
 type rule = {
@@ -68,12 +66,8 @@ let make_domain name data_type label rules assertions =
   { name; data_type; label; rules; assertions }
 
 let fmt_domain ppf domain =
-  let label = match domain.label with
-    | None -> ""
-    | Some label -> Printf.sprintf " (label=%S)" label
-  in
-  Format.fprintf ppf "@[<4>%s : %s%s@ @ %a@ @ %a@]"
-    domain.name domain.data_type label
+  Format.fprintf ppf "@[<hv 4><domain %s@ type=%S%a@ %a@ %a>@]"
+    domain.name domain.data_type (fmt_optional_str "label") domain.label
     (fmt_list "rules" fmt_rule) domain.rules
     (fmt_list "assertions" fmt_assertion) domain.assertions
 
@@ -87,29 +81,68 @@ let make_chassis name implement =
   { name; implement }
 
 let fmt_chassis ppf chassis =
-  Format.fprintf ppf "%s implement %s" chassis.implement chassis.name
+  Format.fprintf ppf "<chassis %s %s implement>" chassis.name chassis.implement
 
 
 type field = {
-  str : string;
+  name : string;
+  domain : string option;
+  data_type : string option;
+  label : string option;
+  rules : rule list;
+  assertions : assertion list;
 }
 
-let make_field str =
-  { str }
+let make_field name domain data_type label rules assertions =
+  { name; domain; data_type; label; rules; assertions }
 
 let fmt_field ppf field =
-  Format.fprintf ppf "%s" field.str
+  Format.fprintf ppf "@[<hv 4><field %s%a%a%a@ %a@ %a>@]"
+    field.name
+    (fmt_optional_str "domain") field.domain
+    (fmt_optional_str "type") field.data_type
+    (fmt_optional_str "label") field.label
+    (fmt_list "rules" fmt_rule) field.rules
+    (fmt_list "assertions" fmt_assertion) field.assertions
+
+
+type response = {
+  name : string;
+}
+
+let make_response name =
+  { name }
+
+let fmt_response ppf response =
+  Format.fprintf ppf "%S" response.name
 
 
 type meth = {
-  str : string;
+  name : string;
+  index : int;
+  synchronous : bool;
+  content : bool;
+  label : string option;
+  rules : rule list;
+  chassiss : chassis list;
+  responses : response list;
+  fields : field list;
+  assertions : assertion list;
+  (* TODO: children *)
 }
 
-let make_meth str =
-  { str }
+let make_meth name index synchronous content label rules chassiss responses fields assertions =
+  { name; index; synchronous; content; label; rules; chassiss; responses; fields; assertions }
 
 let fmt_meth ppf meth =
-  Format.fprintf ppf "%s" meth.str
+  Format.fprintf ppf "@[<hv 4><method %s %d@ synchronous=%B@ content=%B%a@ %a@ %a@ %a@ %a@ %a>@]"
+    meth.name meth.index meth.synchronous meth.content
+    (fmt_optional_str "label") meth.label
+    (fmt_list "rules" fmt_rule) meth.rules
+    (fmt_list "chassis" fmt_chassis) meth.chassiss
+    (fmt_list "responses" fmt_response) meth.responses
+    (fmt_list "fields" fmt_field) meth.fields
+    (fmt_list "assertions" fmt_assertion) meth.assertions
 
 
 type cls = {
@@ -127,7 +160,7 @@ let make_cls name handler index label chassiss methods rules fields =
   { name; handler; index; label; chassiss; methods; rules; fields }
 
 let fmt_cls ppf cls =
-  Format.fprintf ppf  "@[<hv 4><%s (%s) %d@ label=%S@ %a@ %a@ %a@ %a>@]"
+  Format.fprintf ppf "@[<hv 4><class %s (%s) %d@ label=%S@ %a@ %a@ %a@ %a>@]"
     cls.name cls.handler cls.index cls.label
     (fmt_list "chassis" fmt_chassis) cls.chassiss
     (fmt_list "methods" fmt_meth) cls.methods
--- code_gen/spec_parser.ml
+++ code_gen/spec_parser.ml
@@ -3,7 +3,7 @@ type xml_tree =
   | D of string
 
 let in_tree i =
-  let el tag childs = E (tag, childs)  in
+  let el tag children = E (tag, children)  in
   let data d = D d in
   Xmlm.input_doc_tree ~el ~data i
 
@@ -18,6 +18,15 @@ let get_attr name attrs =
   match get_attr_option name attrs with
   | None -> failwith ("Attr not found: " ^ name)
   | Some value -> value
+
+let get_bool_attr name attrs =
+  match get_attr_option name attrs with
+  | None -> false
+  | Some "1" -> true
+  | Some value -> failwith ("Unexpected boolean attr value: " ^ value)
+
+let get_int_attr name attrs =
+  int_of_string @@ get_attr name attrs
 
 let fmt_attr ((_, name), value) =
   Printf.sprintf "%s=%S" name value
@@ -39,22 +48,34 @@ let fmt_elem = function
 let print_elem elem =
   print_endline @@ fmt_elem elem
 
-let parse_constant attrs _children =
+let rec consume_children = function
+  | [] -> ()
+  | D _ :: elems -> consume_children elems
+  | E (((_, "doc"), _), _) :: elems -> consume_children elems
+  | E (tag, _) :: elems -> failwith (Printf.sprintf "bad tag: %s" (fmt_tag tag))
+
+
+let parse_constant attrs children =
   let name = get_attr "name" attrs in
-  let value = int_of_string @@ get_attr "value" attrs in
+  let value = get_int_attr "value" attrs in
   let cls = get_attr_option "class" attrs in
+  let () = consume_children children in
   Amqp_spec.make_constant name value cls
 
-let parse_rule attrs _children =
+let parse_rule attrs children =
   let name = get_attr "name" attrs in
+  let () = consume_children children in
   Amqp_spec.make_rule name
 
-let parse_assertion attrs _children =
-  Amqp_spec.make_assertion (get_attr "check" attrs) (get_attr_option "value" attrs)
+let parse_assertion attrs children =
+  let check = get_attr "check" attrs in
+  let value = get_attr_option "value" attrs in
+  let () = consume_children children in
+  Amqp_spec.make_assertion check value
 
 let parse_domain_children elems =
   let rec inner rules assertions = function
-    | [] -> (rules, assertions)
+    | [] -> (List.rev rules, List.rev assertions)
     | D _ :: elems -> inner rules assertions elems
     | E (((_, "doc"), _), _) :: elems -> inner rules assertions elems
     | E (((_, "rule"), attrs), children) :: elems ->
@@ -75,17 +96,69 @@ let parse_domain attrs children =
 let parse_chassis attrs children =
   let name = get_attr "name" attrs in
   let implement = get_attr "implement" attrs in
+  let () = consume_children children in
   Amqp_spec.make_chassis name implement
 
-let parse_method attrs children =
-  Amqp_spec.make_meth @@ fmt_elem (E ((("", "method"), attrs), children))
+let parse_field_children elems =
+  let rec inner rules assertions = function
+    | [] -> (List.rev rules, List.rev assertions)
+    | D _ :: elems -> inner rules assertions elems
+    | E (((_, "doc"), _), _) :: elems -> inner rules assertions elems
+    | E (((_, "rule"), attrs), children) :: elems ->
+      inner ((parse_rule attrs children) :: rules) assertions elems
+    | E (((_, "assert"), attrs), children) :: elems ->
+      inner rules ((parse_assertion attrs children) :: assertions) elems
+    | E (tag, _) :: elems -> failwith (Printf.sprintf "bad tag: %s" (fmt_tag tag))
+  in
+  inner [] [] elems
 
 let parse_field attrs children =
-  Amqp_spec.make_field @@ fmt_elem (E ((("", "field"), attrs), children))
+  let name = get_attr "name" attrs in
+  let domain = get_attr_option "domain" attrs in
+  let data_type = get_attr_option "type" attrs in
+  let label = get_attr_option "label" attrs in
+  let rules, assertions = parse_field_children children in
+  Amqp_spec.make_field name domain data_type label rules assertions
+
+let parse_response attrs children =
+  let name = get_attr "name" attrs in
+  let () = consume_children children in
+  Amqp_spec.make_response name
+
+let parse_method_children elems =
+  let rec inner rules chassiss responses fields assertions = function
+    | [] -> (List.rev rules, List.rev chassiss, List.rev responses, List.rev fields,
+             List.rev assertions)
+    | D _ :: elems -> inner rules chassiss responses fields assertions elems
+    | E (((_, "doc"), _), _) :: elems -> inner rules chassiss responses fields assertions elems
+    | E (((_, "rule"), attrs), children) :: elems ->
+      inner ((parse_rule attrs children) :: rules) chassiss responses fields assertions elems
+    | E (((_, "chassis"), attrs), children) :: elems ->
+      inner rules ((parse_chassis attrs children) :: chassiss) responses fields assertions elems
+    | E (((_, "response"), attrs), children) :: elems ->
+      inner rules chassiss ((parse_response attrs children) :: responses) fields assertions elems
+    | E (((_, "field"), attrs), children) :: elems ->
+      inner rules chassiss responses ((parse_field attrs children) :: fields) assertions elems
+    | E (((_, "assert"), attrs), children) :: elems ->
+      inner rules chassiss responses fields ((parse_assertion attrs children) :: assertions) elems
+
+    | E (tag, _) :: elems -> failwith (Printf.sprintf "bad tag: %s" (fmt_tag tag))
+  in
+  inner [] [] [] [] [] elems
+
+let parse_method attrs children =
+  let name = get_attr "name" attrs in
+  let index = get_int_attr "index" attrs in
+  let synchronous = get_bool_attr "synchronous" attrs in
+  let content = get_bool_attr "content" attrs in
+  let label = get_attr_option "label" attrs in
+  let rules, chassiss, responses, fields, assertions = parse_method_children children in
+  Amqp_spec.make_meth
+    name index synchronous content label rules chassiss responses fields assertions
 
 let parse_cls_children elems =
   let rec inner chassiss methods rules fields = function
-    | [] -> (chassiss, methods, rules, fields)
+    | [] -> (List.rev chassiss, List.rev methods, List.rev rules, List.rev fields)
     | D _ :: elems -> inner chassiss methods rules fields elems
     | E (((_, "doc"), _), _) :: elems -> inner chassiss methods rules fields elems
     | E (((_, "chassis"), attrs), children) :: elems ->
@@ -103,14 +176,14 @@ let parse_cls_children elems =
 let parse_cls attrs children =
   let name = get_attr "name" attrs in
   let handler = get_attr "handler" attrs in
-  let index = int_of_string @@ get_attr "index" attrs in
+  let index = get_int_attr "index" attrs in
   let label = get_attr "label" attrs in
   let chassiss, methods, rules, fields = parse_cls_children children in
   Amqp_spec.make_cls name handler index label chassiss methods rules fields
 
 let parse_amqp_children elems =
   let rec inner constants domains classes = function
-    | [] -> (constants, domains, classes)
+    | [] -> (List.rev constants, List.rev domains, List.rev classes)
     | D _ :: elems -> inner constants domains classes elems
     | E (((_, "constant"), attrs), children) :: elems ->
       inner ((parse_constant attrs children) :: constants) domains classes elems
@@ -123,9 +196,9 @@ let parse_amqp_children elems =
   inner [] [] [] elems
 
 let parse_amqp attrs children =
-  let major = int_of_string @@ get_attr "major" attrs in
-  let minor = int_of_string @@ get_attr "minor" attrs in
-  let revision = int_of_string @@ get_attr "revision" attrs in
+  let major = get_int_attr "major" attrs in
+  let minor = get_int_attr "minor" attrs in
+  let revision = get_int_attr "revision" attrs in
   let comment = get_attr "comment" attrs in
   let constants, domains, classes = parse_amqp_children children in
   Amqp_spec.make_spec (major, minor, revision) comment constants domains classes

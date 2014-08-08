7642213592835daa0ca74458a76d875a7e2c9dfa
Jeremy Thurgood <firxen@gmail.com>
2014-08-08 14:48:08 +0200

    More informative generated code check failure output.

--- check-generated-code.sh
+++ check-generated-code.sh
@@ -7,6 +7,11 @@
 git status  # For the CI log.
 
 if [ -n "$(git status -z lib/)" ]; then
+    echo ""
+    echo "Generated code does not match repo!"
+    echo ""
+    git diff lib/
+    echo ""
     echo "Generated code does not match repo!"
     exit 1
 fi
